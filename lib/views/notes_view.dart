700c00dd190dcfc7a596bbb5fd8f35e01dcbf423
Mansourx83 <yousaf83000@gmail.com>
2024-10-03 21:15:41 +0300

    create fetch note method

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/custom_floating_button.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
@@ -7,9 +9,12 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NotesViewBody(),
-      floatingActionButton: CustomFloatingButton(),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: NotesViewBody(),
+        floatingActionButton: CustomFloatingButton(),
+      ),
     );
   }
 }
